b0b57ac56742309f51b8fc295d8930f9b9e877df
xiarain <452271058@qq.com>
2017-05-05 16:39:17 +0800

    update the .gitignore

--- build/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/xiarui/catkin_ws/src/droneTest/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
